09db0f9657812badf14ee36cddfbb4a542335311
lsitec-camargo <andressa.camargo@lsitec.org.br>
2022-05-23 16:49:54 -0300

    Initial commit

---
+++ app/src/main/java/br/org/lsitec/android/quizx/MainActivity.java
@@ -0,0 +1,14 @@
+package br.org.lsitec.android.quizx;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
